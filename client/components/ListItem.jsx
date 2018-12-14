bc6308954cf5754f43ffb5d71fc24aa8f33e1c25
Kevin <kevin@cerda.io>
2018-12-14 15:20:44 -0800

    Initial commit, basic skeleton.

---
+++ client/components/ListItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ListItem = props => {
+  return (
+    <div className="result-item">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sed sit
+        facere eligendi asperiores aut laboriosam minima nisi quos nihil
+        repudiandae alias reprehenderit libero in deserunt ullam eos, at ipsum!
+      </p>
+    </div>
+  );
+};
+
+export default ListItem;
