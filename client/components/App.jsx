2fa3383f6256cce3dd97a32d1add4e29cad57fc0
Kevin <kevin@cerda.io>
2018-12-14 12:30:56 -0800

    App is now fetching and console logging data.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.fetchLocationData = this.fetchLocationData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchLocationData();
+  }
+
+  fetchLocationData() {
+    const URI = 'https://my.api.mockaroo.com/locations.json?key=a45f1200';
+    fetch(URI)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(`Error fetching data ${error}`);
+      });
+  }
+
   render() {
     return (
       <div>
