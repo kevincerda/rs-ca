47718db3dded781222c5553bcfc065663a57892a
Kevin <kevin@cerda.io>
2018-12-14 11:58:28 -0800

    Add: server to serve static files.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.listen(port, err => {
   err
     ? console.err(`Error launching server ${err}`)
