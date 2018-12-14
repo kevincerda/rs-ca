ce878c5f9effbb12c8a1f86eb426cfba9834aad2
Kevin <kevin@cerda.io>
2018-12-14 12:18:01 -0800

    Import bootstrap min.css

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../public/styles/styles.css';
 import App from './components/App.jsx';
 
 render(<App />, document.getElementById('root'));
