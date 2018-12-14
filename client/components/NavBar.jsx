fd30d3e9a7a61b17b79dacfb83b31f350df43cdc
Kevin <kevin@cerda.io>
2018-12-14 15:21:22 -0800

    Removed container wrap.

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -3,32 +3,30 @@ import Logo from '../../assets/rioseo-logo.png';
 
 const NavBar = props => (
   <nav className="navbar navbar-expand-lg navbar-light" id="navbar">
-    <div className="container">
-      <a href="/" className="navbar-brand">
-        <img src={Logo} />
-      </a>
-      <button
-        className="navbar-toggler float-right"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbar"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="navbar-collapse collapse" id="navbar">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Menu
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Truck Locator
-            </a>
-          </li>
-        </ul>
-      </div>
+    <a href="/" className="navbar-brand">
+      <img src={Logo} />
+    </a>
+    <button
+      className="navbar-toggler float-right"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbar"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="navbar-collapse collapse" id="navbar">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Menu
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Truck Locator
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 );
