a2f07cc76b68a7bc642b43689ed4b04a9807b9f0
Kevin <kevin@cerda.io>
2018-12-14 14:18:16 -0800

    Add support to jpeg images.

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpe?g|gif)$/,
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
