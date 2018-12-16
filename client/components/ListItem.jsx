043cd74a75ab54d22d8b80f107688eb9ca1d8045
Kevin <kevin@cerda.io>
2018-12-16 13:26:45 -0800

    Add activeItem conditional rendering, will change bg style on active items.

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const ListItem = props => {
   const transformedData = props.data.map((truck, index) => {
     return (
-      <div className="col-10 offset-1 result-item" key={truck.id}>
+      <div
+        className={
+          props.activeItem === truck.id
+            ? 'col-10 offset-1 result-item active-item'
+            : 'col-10 offset-1 result-item'
+        }
+        key={truck.id}
+        data-id={truck.id}
+        data-latitude={truck.latitude}
+        data-longitude={truck.longitude}
+        onClick={props.handleItemClick}
+      >
         <div className="row">
           <div className="col-6">{truck.name}</div>
           <div className="col-6">0.5 mi</div>
@@ -14,12 +25,12 @@ const ListItem = props => {
           <div className="col-12">Open until {}</div>
           <div className="col-12">123-456-7890</div>
           <div className="col-6">
-            <button type="button" class="btn btn-dark btn-block">
+            <button type="button" className="btn btn-dark btn-block">
               Directions
             </button>
           </div>
           <div className="col-6">
-            <button type="button" class="btn btn-dark btn-block">
+            <button type="button" className="btn btn-dark btn-block">
               More Info
             </button>
           </div>
@@ -31,13 +42,12 @@ const ListItem = props => {
     <div className="col-5" id="list">
       <div className="row">
         <div className="col-12">
-          <h5>Viewing All {props.data.length} Taco Trucks</h5>
+          <blockquote className="blockquote text-center">
+            <h5>Found {props.data.length} Taco Trucks Near You</h5>
+          </blockquote>
         </div>
       </div>
-      <div className="row">
-        {transformedData[0]}
-        {transformedData[1]}
-      </div>
+      <div className="row">{transformedData}</div>
     </div>
   );
 };
