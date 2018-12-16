09f2c3a34449f28f71f481749e37c958b6207011
Kevin <kevin@cerda.io>
2018-12-16 11:38:23 -0800

    Implemented conditional rendering to display default message when no marker location is set.

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ListItem = props => {
   const transformedData = props.data.map((truck, index) => {
     return (
-      <div className="col-3 card result-item" key={truck.id}>
+      <div className="col-10 offset-1 result-item" key={truck.id}>
         <div className="row">
           <div className="col-6">{truck.name}</div>
           <div className="col-6">0.5 mi</div>
@@ -11,15 +11,33 @@ const ListItem = props => {
           <div className="col-12">
             {truck.city}, {truck.state} {truck.postal_code}
           </div>
-          <div className="col-12">Open until 9pm</div>
+          <div className="col-12">Open until {}</div>
           <div className="col-12">123-456-7890</div>
+          <div className="col-6">
+            <button type="button" class="btn btn-dark btn-block">
+              Directions
+            </button>
+          </div>
+          <div className="col-6">
+            <button type="button" class="btn btn-dark btn-block">
+              More Info
+            </button>
+          </div>
         </div>
       </div>
     );
   });
   return (
     <div className="col-5" id="list">
-      <div className="row">{transformedData}</div>
+      <div className="row">
+        <div className="col-12">
+          <h5>Viewing All {props.data.length} Taco Trucks</h5>
+        </div>
+      </div>
+      <div className="row">
+        {transformedData[0]}
+        {transformedData[1]}
+      </div>
     </div>
   );
 };
