a08bd7298798ce14b4b04b0437289ec311e05976
Kevin <kevin@cerda.io>
2018-12-16 19:58:55 -0800

    Add footer component, renders on displays smaller than MD.

---
+++ client/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = props => (
+  <footer className="footer d-lg-none">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-6 footer-tab tab-active">List</div>
+        <div className="col-6 footer-tab">Map</div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
