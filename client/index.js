ae622cd19abc60d21488d4962bb5819d7a6cb7ed
Kevin <kevin@cerda.io>
2018-12-16 20:37:09 -0800

    Import FA.

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/styles/styles.css';
+import '@fortawesome/fontawesome-free';
 import App from './components/App.jsx';
 
 render(<App />, document.getElementById('root'));
